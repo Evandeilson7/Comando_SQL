a54cfde17100876f226be6024fcefffeb36eb744
Evandeilson silva <evandeilson2011@hotmail.com>
2025-04-07 22:29:37 -0300

    Update exercicioprontosql.sql

--- exercicioprontosql.sql
+++ exercicioprontosql.sql
@@ -27,3 +27,46 @@ CREATE TABLE ALUNO (
 -- Agora pode inserir os alunos
 INSERT INTO ALUNO (id, matricula, nome, curso) VALUES
 ('123', '123456', 'BIANCA MARIA PEDROSA', 'ma');  -- 'ma' deve existir na tabela CURSO
+('212','21234','TATIANE CITTON','AS‘),
+('221','22145','ALEXANDRE PEDROSA ','
+('231','23144','ALEXANDREMONTEIRO','CC‘),
+('321','32111','MARCIA RIBEIRO ','
+('661','66123','JUSSARA MARANDOLA','SI'),
+('765','76512','WALTERRODRIGUES','SI');
+
+INSERT INTO
+aluno (RA,RG,NOME<CODCURSO)
+
+insert into CURSO VALUES
+('AS','ANALISE DE SISTEMAS',1000);
+
+insert into CURSO VALUES
+('CC','CIENCIA
+DA COMPUTACAO',950);
+
+    insert into DISCIPLINA values
+('BD','BANCO
+DE DADOS','CC',4);
+>
+insert into D ISCIPLINA values
+('BDA','BANCO
+DE DADOS AVANCADOS','CC',6);
+>
+insert into D ISCIPLINA values
+('BDOO','BANCO
+DE DADOS O OBJETOS','SI',4);
+>
+insert into D ISCIPLINA values
+('BDS','SISTEMAS
+DE BANCO DE DADOS','AS', 4);
+>
+insert into DISCIPLINA values
+('DBD','DESENVOLVIMENTO
+BANCO DE DADOS','SI',6);
+>
+insert into D ISCIPLINA values
+('IBD','INTROD
+UC AO A BANCO D E DADOS','AS',2);
+
+insert into CURSO V AL U ES
+('SI','SISTEMAS DE INFORMACAO',800);
